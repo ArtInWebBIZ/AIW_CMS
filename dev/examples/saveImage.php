4d8959f9d6024c4fdd8296b82742de7f1645e25d
ArtInWebBiz <kruk.ter@gmail.com>
2025-05-03 11:07:45 +0300

    V 1.06

--- dev/examples/saveImage.php
+++ dev/examples/saveImage.php
@@ -7,31 +7,40 @@
  * @license    GNU General Public License version 3 - see LICENSE.txt
  */
 
+namespace PasteNameSpace; // VALUE IS EXAMPLE
+
+use Core\Config;
+
 defined('AIW_CMS') or die;
 
-private $saveIntroImage = 'null';
+class ClassName // VALUE IS EXAMPLE
+{
+
+    private $saveIntroImage = null;
     /**
      * Save to server intro image
+     * Return if success file name in key ['file_name']
      * Return if success empty key ['msg']
      * Return if error empty key ['file_name']
      * @return array
      */
     private function saveIntroImage(): array
     {
-        if ($this->saveIntroImage == 'null') {
+        if ($this->saveIntroImage === null) {
 
             $this->saveIntroImage = (new \Core\Plugins\Upload\OneImage)->uploadImage(
                 [
                     'form_value'        => $this->checkForm(), // Get form values
-                    'input_name'        => 'intro_img', // Get images files
-                    'items_date'        => $this->newPostTime(), // 
-                    'upload_dir_name'   => Config::getCfg('CFG_INTRO_IMAGE_PATH'), // 
-                    'items_id_or_code'  => $this->saveNewItem(), // 
-                    'img_width'         => Config::getCfg('CFG_INTRO_IMAGE_WIDTH'), // 
-                    'img_height'        => Config::getCfg('CFG_INTRO_IMAGE_HEIGHT'), // 
+                    'input_name'        => 'intro_img', // Get images files // VALUE IS EXAMPLE
+                    'items_date'        => 'items_date_value', // VALUE IS EXAMPLE !!! CHANGE_THIS !!!
+                    'upload_dir_name'   => Config::getCfg('CFG_INTRO_IMAGE_PATH'), // VALUE IS EXAMPLE
+                    'items_id_or_code'  => 'items_id_or_code_value', // VALUE IS EXAMPLE !!! CHANGE_THIS !!!
+                    'img_width'         => Config::getCfg('CFG_INTRO_IMAGE_WIDTH'), // VALUE IS EXAMPLE
+                    'img_height'        => Config::getCfg('CFG_INTRO_IMAGE_HEIGHT'), // VALUE IS EXAMPLE
                 ]
             );
         }
 
         return $this->saveIntroImage;
     }
+}
